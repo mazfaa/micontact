c85552ca88c89ce0ad1f6979e16fbcb6af244315
Muhammad Azfa Asykarulloh <kangazfa@gmail.com>
2023-04-11 01:25:31 +0700

    [FIX] fix deletion

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -37,7 +37,7 @@
                   @csrf
                   @method('delete')
                   <a href="{{ route('contact.edit', $contact) }}" class="btn btn-sm btn-success"><i class="bi bi-pencil-square"></i></a>
-                  <button type="submit" class="btn btn-sm btn-danger d-inline" onclick="confirm('Are you sure want to delete this contact?')"><i class="bi bi-trash3"></i></button>
+                  <button type="submit" class="btn btn-sm btn-danger d-inline" onclick="return confirm('Are you sure want to delete this contact?')"><i class="bi bi-trash3"></i></button>
                 </form>
               </td>
             </tr>
